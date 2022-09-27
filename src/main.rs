e91bf4ff24d31149d99fda28638d03ea42bdf5ad
tatamim <646taoufik@gmail.com>
2022-09-27 22:30:40 +0100

    fix(main): fix bug when stdin fails

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() -> Result<()> {
             let mut str = String::new();
             if stdin().read_line(&mut str).is_err() {
                 println!("Could not read the line");
+                continue;
             }
             match str
                 .trim()
